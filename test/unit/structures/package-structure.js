17d34f32ad10420202e5f62c56bb8246121e7da1
Aaron Marais <its_me@aaronleem.co.za>
2022-01-27 18:25:47 +0200

    minor: clean up code & generate dist

--- test/unit/structures/package-structure.js
+++ test/unit/structures/package-structure.js
@@ -1,8 +1,6 @@
 import chai, { expect } from "chai";
 import chaiAsPromised from "chai-as-promised";
 import sinonChai from "sinon-chai";
-import mockFs from "mock-fs";
-import sinon from "sinon";
 
 chai.use(sinonChai);
 chai.use(chaiAsPromised);
